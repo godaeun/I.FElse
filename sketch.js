6db0d56f63401e5e8dd2173f7bb9bf08ba3ff592
godaeun <gowooda@naver.com>
2014-10-02 16:29:16 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -7,11 +7,19 @@ function setup()
 
   function draw()
 {
-  background(0);
-  if (mouseX < 300) {
-  ellipse(200,200,100,100);
-}
-else {
-  ellipse(400, 200, 100, 100);
+  background(25,125,25);
+  // draw function
+if (mouseX < 100) {
+  ellipse(50, 200, 100, 100);
+} else if (mouseX < 200){
+  ellipse(150, 200, 100, 100);
+} else if (mouseX < 300){
+  ellipse(250, 200, 100, 100);
+} else if (mouseX < 400){
+  ellipse(350, 200, 100, 100);
+} else if (mouseX < 500){
+  ellipse(450, 200, 100, 100);
+} else {
+  ellipse(550, 200, 100, 100);
 }
 }
