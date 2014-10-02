17f537f4f000100f63b26011f1868ce0f5a43f6a
godaeun <gowooda@naver.com>
2014-10-02 16:49:11 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -11,39 +11,39 @@ function setup()
 
   // draw function
 if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
+  ellipse(50, 300, 100, 100);
 } else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
+  ellipse(150, 300, 100, 100);
 } else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
+  ellipse(260, 300, 100, 100);
 } else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
+  ellipse(370, 300, 100, 100);
 } else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
+  ellipse(480, 300, 100, 100);
 } else if (mouseX < 600){
-  ellipse(550, 200, 100, 100);
+  ellipse(570, 300, 100, 100);
 } else if (mouseX < 700){
-  ellipse(650, 200, 100, 100);    
+  ellipse(650, 300, 100, 100);    
 } else {
-  ellipse(750, 200, 100, 100);
+  ellipse(750, 300, 100, 100);
 }
 
 if (mouseX < 100) {
-  rect(0, 150, 100, 100);
+  rect(0, 250, 100, 100);
 } else if (mouseX < 200){
-  rect(10, 150, 100, 100);
+  rect(10, 250, 100, 100);
 } else if (mouseX < 300){
-  rect(100, 150, 100, 100);
+  rect(100, 250, 100, 100);
 } else if (mouseX < 400){
-  rect(200, 150, 100, 100);
+  rect(200, 250, 100, 100);
 } else if (mouseX < 500){
-  rect(300, 150, 100, 100);
+  rect(300, 250, 100, 100);
 } else if (mouseX < 600){
-  rect(400, 150, 100, 100);
+  rect(400, 250, 100, 100);
 } else if (mouseX < 700){
-  rect(500, 150, 100, 100);    
+  rect(500, 250, 100, 100);    
 } else {
-  rect(700, 150, 100, 100);
+  rect(700, 250, 100, 100);
 }
 
 }
