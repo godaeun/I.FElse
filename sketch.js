042ef15fa284318ba13b7a643b67c70698913236
godaeun <gowooda@naver.com>
2014-10-02 16:38:45 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -22,4 +22,19 @@ if (mouseX < 100) {
 } else {
   ellipse(550, 200, 100, 100);
 }
+
+if (mouseX < 100) {
+  rect(0, 150, 100, 100);
+} else if (mouseX < 200){
+  rect(10, 150, 100, 100);
+} else if (mouseX < 300){
+  rect(100, 150, 100, 100);
+} else if (mouseX < 400){
+  rect(200, 150, 100, 100);
+} else if (mouseX < 500){
+  rect(300, 150, 100, 100);
+} else {
+  rect(500, 150, 100, 100);
+}
+
 }
