07e7cc6175ec360b08e024b5319884f3be598c43
godaeun <gowooda@naver.com>
2014-10-02 17:05:35 +0900

    마지막

--- sketch.js
+++ sketch.js
@@ -29,22 +29,28 @@ if (mouseX < 100) {
 }
 
 if (mouseX < 100) {
-  fill(249,229,158)
+  fill(125,105,244)
   rect(0, 250, 100, 100);
 } else if (mouseX < 200){
+  fill(0,134,255)	
   rect(10, 250, 100, 100);
 } else if (mouseX < 300){
+  fill(0,134,255)	
   rect(100, 250, 100, 100);
 } else if (mouseX < 400){
+  fill(0,134,255)	
   rect(200, 250, 100, 100);
 } else if (mouseX < 500){
+  fill(0,134,255)	
   rect(300, 250, 100, 100);
 } else if (mouseX < 600){
+  fill(0,134,255)	
   rect(400, 250, 100, 100);
 } else if (mouseX < 700){
+  fill(0,134,255)	
   rect(500, 250, 100, 100);    
 } else {
-  fill(249,229,158)
+  fill(125,105,244)
   rect(700, 250, 100, 100);
 }
 
