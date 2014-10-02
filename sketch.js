7b1458405a6586ab339f585d9f0ceecab528dbb2
godaeun <gowooda@naver.com>
2014-10-02 16:19:12 +0900

    gg

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 function setup()
 {
 	createCanvas(680, 600);
-	noFill();
-	stroke(255, 0, 0);
+	background(0);	
 
 }
 
 function draw()
 {
 	background(0);
+	// draw function
+	if (mouseX < 300) {
+   ellipse(200, 200, 100, 100);
+}
+	noFill();
+	stroke(255, 0, 0);
 	ellipse(280, 260, mouseX, mouseY);	
 }
