e9ad0b9cc8d4256a0e1ca9f8ccda08c5cf545653
godaeun <gowooda@naver.com>
2014-10-02 16:46:17 +0900

    ffd

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 function setup()
  {
   createCanvas(800, 600);
-  noFill();
-  stroke(255, 0, 0);
 }
 
   function draw()
 {
-  background(25,125,25);
+  background(63,170,150);
+  noStroke();   
+  fill(222,86,74)
+
   // draw function
 if (mouseX < 100) {
   ellipse(50, 200, 100, 100);
@@ -19,8 +20,12 @@ if (mouseX < 100) {
   ellipse(350, 200, 100, 100);
 } else if (mouseX < 500){
   ellipse(450, 200, 100, 100);
-} else {
+} else if (mouseX < 600){
   ellipse(550, 200, 100, 100);
+} else if (mouseX < 700){
+  ellipse(650, 200, 100, 100);    
+} else {
+  ellipse(750, 200, 100, 100);
 }
 
 if (mouseX < 100) {
@@ -33,8 +38,12 @@ if (mouseX < 100) {
   rect(200, 150, 100, 100);
 } else if (mouseX < 500){
   rect(300, 150, 100, 100);
+} else if (mouseX < 600){
+  rect(400, 150, 100, 100);
+} else if (mouseX < 700){
+  rect(500, 150, 100, 100);    
 } else {
-  rect(500, 150, 100, 100);
+  rect(700, 150, 100, 100);
 }
 
 }
